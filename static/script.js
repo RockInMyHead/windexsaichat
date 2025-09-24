19a8c994082d9fdb0f9c9258ea073acefacd1e4e
Artem Butko <artembutko@Mac.lan>
2025-09-23 23:23:30 -0700

    Fix moon icon visibility by adding fill attribute

--- static/script.js
+++ static/script.js
@@ -389,7 +389,7 @@ class WindexAI {
             } else {
                 // Light theme - show moon icon
                 icon.innerHTML = `
-                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    <path fill="currentColor" d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
                 `;
                 console.log(`Set moon icon for button ${index + 1}`);
             }
